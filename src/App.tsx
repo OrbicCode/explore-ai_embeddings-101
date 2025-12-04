d6ba4c40c2fd84a8211b14d196c712005c9c3f94
James O'Kane <orbiccode@gmail.com>
2025-12-04 12:25:09 +0000

    handleClick function to get embeddings

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
+import OpenAI from 'openai';
 import './App.css';
 
+const openai = new OpenAI({
+  apiKey: import.meta.env.VITE_OPENAI_API_KEY,
+  dangerouslyAllowBrowser: true,
+});
+
 function App() {
+  async function handleClick() {
+    const response = await openai.embeddings.create({
+      model: 'text-embedding-ada-002',
+      input: 'Lord of the rings is the best!',
+    });
+    console.log(response.data[0].embedding);
+  }
+
   return (
     <main>
       <h1>AI Embeddings 101</h1>
+      <button onClick={handleClick}>Get Embeddings</button>
     </main>
   );
 }
